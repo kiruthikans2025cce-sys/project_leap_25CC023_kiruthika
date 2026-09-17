ba22109dbaf346de15bbcec4483b5160f431f254
kiruthikans2025cce-sys <kiruthika.ns2025cce@sece.ac.in>
2026-09-17 15:16:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.example.project_leap_25cc023_kiruthika.Controller;
+
+/*public class Main {
+    public static void main(String [] args){
+        WebController wc=new WebController();
+        System.out.println(wc.calculate());
+    }
+}*/
